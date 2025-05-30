ef4104cf32e0b5a57ca7a6c9eccd634e3fa81e53
Kartikeya Dangat <kartikeya.dangat@mphasis.com>
2025-05-30 13:54:24 +0530

    Added RM Onboarding microservice initial structure

---
+++ src/test/java/com/Mphasis/RMOnboarding/rm_onboarding_service/RmOnboardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mphasis.RMOnboarding.rm_onboarding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmOnboardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
